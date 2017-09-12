0b9cb55e0b4c90a0cc9dbae2b7da3a0004b84eb5
daigoishere <daigo.cd@gmail.com>
2017-09-12 12:16:23 +1000

    Create pixelColor.c

---
+++ pixelColor.c
@@ -0,0 +1,27 @@
+// Assignment 1 Mandelbrot: Pixel Color
+//
+// Completed by
+//  ... (z0000000)
+//  ... (z0000000)
+//
+// Modified on 2017-??-??
+// Tutor's name (dayHH-lab)
+
+#include "mandelbrot.h"
+#include "pixelColor.h"
+
+// This function should take a number (of steps taken to escape the
+// Mandelbrot set), and convert it to a color.
+//
+// You have artistic licence here, so make your Mandelbrot look amazing.
+pixel pixelColor(int steps) {
+    pixel color = {
+        .red = 0,
+        .green = 0,
+        .blue = 0
+    };
+
+    // COMPLETE THIS FUNCTION
+
+    return color;
+}
